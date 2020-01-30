696cbe1571d696cdcd529380fff0cde81c8b08cb
jeteraguilar <jeteraguilar@gmail.com>
2020-01-30 19:06:05 -0300

    Controllers Resources

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,4 +12,28 @@ class ProductController extends Controller
 
         return $products;
     }
+    public function show($id)
+    {
+        return "Exibindo o produto do id: {$id}";
+    }
+    public function create()
+    {
+        return 'Exibindo o form de cadastro de um novo produto';
+    }
+    public function edit($id)
+    {
+        return "Form para editar o produto: {$id}";
+    }
+    public function store()
+    {
+        return 'Cadastrando um novo produto';
+    }
+    public function update($id)
+    {
+        return "Editando o produto: {$id}";
+    }
+    public function destroy($id)
+    {
+        return "Deletando o produto: {$id}";
+    }
 }
